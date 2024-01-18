acdcab4d13d4be4fbe774b95559bb66685d54633
unknown <memorytemp5@gmail.com>
2024-01-18 21:57:58 +0530

    Add Date Period

--- node_learn_project/relationshipProject/controllers/SearchController.js
+++ node_learn_project/relationshipProject/controllers/SearchController.js
@@ -77,7 +77,7 @@ const onInputSearchSecond = async (req, res) => {
             .orWhere('web_dev.name', 'like', `%${search_query}%`)
             .orWhere('web_dev.email', 'like', `%${search_query}%`)
             .orWhere('desig.designation_name', 'like', `%${search_query}%`)
-            .leftJoin('designations as desig', 'desig.id', '=','web_dev.designation')
+            .leftJoin('designations as desig', 'desig.id', '=', 'web_dev.designation')
             .select(
                 'web_dev.*',
                 'desig.designation_name'
@@ -89,8 +89,23 @@ const onInputSearchSecond = async (req, res) => {
         message: data
     });
 }
+// Get Dates Between Two dates
+const getDatesPeriods = async (req, res) => {
+    var start_date = new Date('2022-01-01');
+    var end_date = new Date('2022-03-10');
+    var current_date = new Date(start_date);
+    while (current_date <= end_date) {
+        console.log(current_date.toISOString().substring(0, 10));
+        console.log(current_date.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '-'));
+        current_date.setDate(current_date.getDate() + 1);
+    }
+    return await res.status(200).json({
+        message: "Ok"
+    });
+}
 module.exports = {
     addData,
     onInputSearchFrist,
-    onInputSearchSecond
+    onInputSearchSecond,
+    getDatesPeriods
 }
--- node_learn_project/relationshipProject/routes/SearchRoutes.js
+++ node_learn_project/relationshipProject/routes/SearchRoutes.js
@@ -5,4 +5,5 @@ const SearchController = require('../controllers/SearchController');
 router.get('/search-add-data', SearchController.addData);
 router.post('/on-input-search-frist', SearchController.onInputSearchFrist);
 router.post('/on-input-search-second', SearchController.onInputSearchSecond);
+router.get('/get-dates-periods', SearchController.getDatesPeriods);
 module.exports = router;
